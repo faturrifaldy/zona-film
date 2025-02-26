0cac0aa44e2322f4c58266934d027b26eec51798
Moh.afizan <alfijan12@gmail.com>
2025-02-27 00:13:17 +0800

    modification footer section

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,23 +1,24 @@
-'use strict';
+"use strict";
 
 import { api_key, fetchDataFromServer } from "./api.js";
 
 export function sidebar() {
-    const genreList = {};
+  const genreList = {};
 
-    fetchDataFromServer(`https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}`, function ({ genres }) {
+  fetchDataFromServer(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}`,
+    function ({ genres }) {
+      for (const { id, name } of genres) {
+        genreList[id] = name;
+      }
 
-        for (const { id, name } of genres) {
-            genreList[id] = name;
-        }
+      genreLink();
+    }
+  );
 
-        genreLink();
-
-    });
-
-    const sidebarInner = document.createElement("div");
-    sidebarInner.classList.add("sidebar-inner");
-    sidebarInner.innerHTML = `
+  const sidebarInner = document.createElement("div");
+  sidebarInner.classList.add("sidebar-inner");
+  sidebarInner.innerHTML = `
     <div class="sidebar-list">
 
         <p class="title">Genre</p>
@@ -31,48 +32,48 @@ export function sidebar() {
     </div>
 
     <div class="sidebar-footer">
-        <p class="copyright">Copyright 2023</p>
+        <p class="copyright">Copyright 2025</p>
+        <p class="copyright">Faturrahman Rifaldy_F52121042</p>
 
         <img src="./assets/images/tmdb-logo.svg" width="130" height="17" alt="the movie database logo">
     </div>`;
 
+  const genreLink = function () {
+    for (const [genreId, genreName] of Object.entries(genreList)) {
+      const link = document.createElement("a");
+      link.classList.add("sidebar-link");
+      link.setAttribute("href", "./movie-list.html");
+      link.setAttribute("menu-close", "");
+      link.setAttribute(
+        "onclick",
+        `getMovieList("with_genres=${genreId}", "${genreName}")`
+      );
 
-    const genreLink = function() {
-
-        for (const [genreId, genreName] of Object.entries(genreList)) {
-
-            const link = document.createElement("a");
-            link.classList.add("sidebar-link");
-            link.setAttribute("href", "./movie-list.html");
-            link.setAttribute("menu-close", "");
-            link.setAttribute("onclick", `getMovieList("with_genres=${genreId}", "${genreName}")`);
-
-            link.textContent = genreName;
-            sidebarInner.querySelectorAll(".sidebar-list")[0].appendChild(link);
-        }
-
-        const sidebar = document.querySelector("[sidebar]");
-        sidebar.appendChild(sidebarInner);
-        toggleSidebar(sidebar);
+      link.textContent = genreName;
+      sidebarInner.querySelectorAll(".sidebar-list")[0].appendChild(link);
     }
 
-    const toggleSidebar = function(sidebar) {
-        const sidebarBtn = document.querySelector("[menu-btn]");
-        const sidebarTogglers = document.querySelectorAll("[menu-toggler]");
-        const sidebarClose = document.querySelectorAll("[menu-close]");
-        const overlay = document.querySelector("[overlay]");
+    const sidebar = document.querySelector("[sidebar]");
+    sidebar.appendChild(sidebarInner);
+    toggleSidebar(sidebar);
+  };
 
-        addEventOnElements(sidebarTogglers, "click", function(){
-            sidebar.classList.toggle("active");
-            sidebarBtn.classList.toggle("active");
-            overlay.classList.toggle("active");
-        });
+  const toggleSidebar = function (sidebar) {
+    const sidebarBtn = document.querySelector("[menu-btn]");
+    const sidebarTogglers = document.querySelectorAll("[menu-toggler]");
+    const sidebarClose = document.querySelectorAll("[menu-close]");
+    const overlay = document.querySelector("[overlay]");
 
-        addEventOnElements(sidebarClose, "click", function(){
-            sidebar.classList.remove("active");
-            sidebarBtn.classList.remove("active");
-            overlay.classList.remove("active");
-        });
-    }
+    addEventOnElements(sidebarTogglers, "click", function () {
+      sidebar.classList.toggle("active");
+      sidebarBtn.classList.toggle("active");
+      overlay.classList.toggle("active");
+    });
 
+    addEventOnElements(sidebarClose, "click", function () {
+      sidebar.classList.remove("active");
+      sidebarBtn.classList.remove("active");
+      overlay.classList.remove("active");
+    });
+  };
 }
